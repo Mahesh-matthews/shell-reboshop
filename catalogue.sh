f38400c9c1fa995a06e246a6e742343a833e501f
Mahesh Kasapu <mahimystu@gmail.com>
2026-07-25 00:34:35 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -78,7 +78,8 @@ dnf install mongodb-mongosh -y &>> $LOGS_FILE
 validate $? "Installing mongodb client"
 
 INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval  'db.getMongo().getDBNames().indexof("catalogue")') &>> $LOGS_FILE
-if [ $INDEX -eq -1 ]; then
+
+if [ $INDEX -le -1 ]; then
        mongosh --host $MONGODB_HOST </app/db/catalogue.js &>> $LOGS_FILE
     validate $? "Loading Products"
  else

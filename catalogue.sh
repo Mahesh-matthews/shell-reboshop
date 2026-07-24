a18ed2f49b47dfe67d981f76a9ee99c127554658
Mahesh Kasapu <mahimystu@gmail.com>
2026-07-25 00:42:53 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -77,7 +77,7 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGS_FILE
 dnf install mongodb-mongosh -y &>> $LOGS_FILE
 validate $? "Installing mongodb client"
 
-INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval  'db.getMongo().getDBNames().indexof("catalogue")') &>> $LOGS_FILE
+INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval  'db.getMongo().getDBNames().indexof("catalogue")')
 
 if [ $INDEX -le 0 ]; then
        mongosh --host $MONGODB_HOST </app/db/master-data.js &>> $LOGS_FILE
